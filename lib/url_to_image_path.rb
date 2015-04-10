a385a14024b27215be3285e298e097f21fcccabb
NevenRakonic <neven1@Nevens-MacBook-Pro.local>
2015-04-10 16:35:31 +0200

    fix path that loads the rake task

--- lib/url_to_image_path.rb
+++ lib/url_to_image_path.rb
@@ -3,7 +3,7 @@ require "url_to_image_path/version"
 module UrlToImagePath
   class Railtie < Rails::Railtie
     rake_tasks do 
-      load '../tasks/convert_urls.rake'      
+      load 'tasks/convert_urls.rake'      
     end
   end
 end

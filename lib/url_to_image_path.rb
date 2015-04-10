35579e3b33a9e9bc1756cdb69b52d931de47a29a
NevenRakonic <neven1@Nevens-MacBook-Pro.local>
2015-04-10 16:40:51 +0200

    further update to railtie implementation

--- lib/url_to_image_path.rb
+++ lib/url_to_image_path.rb
@@ -1,9 +1,5 @@
 require "url_to_image_path/version"
 
 module UrlToImagePath
-  class Railtie < Rails::Railtie
-    rake_tasks do 
-      load 'tasks/convert_urls.rake'      
-    end
-  end
+  require 'url_to_image_path/railtie' if defined?(Rails)
 end
---
+++ lib/url_to_image_path/railtie.rb
@@ -0,0 +1,11 @@
+require 'url_to_image_path'
+require 'rails'
+module MyPlugin
+  class Railtie < Rails::Railtie
+    railtie_name :url_to_image_path
+
+    rake_tasks do
+      load "tasks/convert_urls.rake"
+    end
+  end
+end

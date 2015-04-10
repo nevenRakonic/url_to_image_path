72966fbcb2e064c0cf640e6cc24924208a8fcf0f
NevenRakonic <neven1@Nevens-MacBook-Pro.local>
2015-04-10 17:17:04 +0200

    fixed path and new gem version

--- lib/url_to_image_path/railtie.rb
+++ lib/url_to_image_path/railtie.rb
@@ -1,11 +1,12 @@
 require 'url_to_image_path'
 require 'rails'
+
 module MyPlugin
   class Railtie < Rails::Railtie
     railtie_name :url_to_image_path
 
     rake_tasks do
-      load "./tasks/convert_urls.rake"
+      load "././tasks/convert_urls.rake"
     end
   end
 end

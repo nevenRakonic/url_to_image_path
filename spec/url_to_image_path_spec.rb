69747a1e266c57f7eedceff6440e28857416bfdc
NevenRakonic <neven1@Nevens-MacBook-Pro.local>
2015-04-17 15:50:52 +0200

    test examples

--- spec/url_to_image_path_spec.rb
+++ spec/url_to_image_path_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
+require 'rake'
 
 describe UrlToImagePath do
   it 'has a version number' do
     expect(UrlToImagePath::VERSION).not_to be nil
+  end
+
+  describe "rake task test" do
+    before do
+      load File.expand_path("../../tasks/convert_urls.rb", __FILE__)
+      Rake::Task.define_task(:environment)
+    end
+
+    it ":foo returns foo" do
+      # expect(Foo).to receive :bar
+      # Rake::Task["url:foo"].invoke
+      # Rake::Task["url:foo"].reenable
+      # expect(Rake::Task["url:foo"].invoke).to eq "foo"
+    end
   end
 
 end
